1abe8674a0c40189ffea5298818211d5bae1b993
Jan Lindblom <janlindblom@fastmail.fm>
2022-06-27 22:41:40 +0300

    Setting things up a bit.

--- oled_stats.py
+++ oled_stats.py
@@ -1,26 +1,18 @@
 # SPDX-FileCopyrightText: 2017 Tony DiCola for Adafruit Industries
 # SPDX-FileCopyrightText: 2017 James DeVito for Adafruit Industries
+# SPDX-FileCopyrightText: 2022 Jan Lindblom for Namnløs
 # SPDX-License-Identifier: MIT
-
-# This example is for use on (Linux) computers that are using CPython with
-# Adafruit Blinka to support CircuitPython libraries. CircuitPython does
-# not support PIL/pillow (python imaging library)!
 
 import time
 import subprocess
-
-#from board import SCL, SDA
-#import busio
 import board
-from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
+from PIL import Image, ImageDraw, ImageFont
 
 # Create the I2C interface.
 i2c = board.I2C()
 
 # Create the SSD1306 OLED class.
-# The first two parameters are the pixel width and pixel height.  Change these
-# to the right size for your display!
 disp = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3c)
 
 # Clear display.
@@ -28,7 +20,6 @@ disp.fill(0)
 disp.show()
 
 # Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
 width = disp.width
 height = disp.height
 image = Image.new("1", (width, height))
@@ -39,7 +30,6 @@ draw = ImageDraw.Draw(image)
 # Draw a black filled box to clear the image.
 draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-# Draw some shapes.
 # First define some constants to allow easy resizing of shapes.
 padding = -2
 top = padding
@@ -47,26 +37,18 @@ bottom = height - padding
 # Move left to right keeping track of the current x position for drawing shapes.
 x = 0
 
-# Load default font.
-font = ImageFont.load_default()
-
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the
-# same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+#font = ImageFont.load_default()
 font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 9)
 #font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf', 9)
 #font = ImageFont.truetype('/usr/share/fonts/truetype/ttf-bitstream-vera/Vera.ttf', 9)
 
+# Draws a 'progress bar' style horisontal meter.
 def drawPBar(start, val):
     draw.rectangle((0, top + start, width - 1, top + start + 7), outline=1, fill=0)
     draw.rectangle((1, top + start + 1, (val / 100) * (width - 2), top + start + 6), outline=0, fill=1)
 
 while True:
-    # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
-
-    # Shell scripts for system monitoring from here:
-    # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
     cmd = "hostname -I | cut -d' ' -f1"
     IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
     cmd = 'cut -f 1 -d " " /proc/loadavg'
@@ -80,17 +62,14 @@ while True:
     cmd = "vcgencmd measure_temp |cut -f 2 -d '='"
     Temp = subprocess.check_output(cmd, shell=True).decode("utf-8")
 
-    # Write four lines of text.
     draw.text((x, top + 0), "IP: " + IP, font=font, fill=128)
     draw.text((x, top + 9), "CPU load: " + CPU, font=font, fill=255)
     drawPBar(20, int(float(CPU) * 100))
     draw.text((x, top + 27), MemUsage, font=font, fill=255)
-    drawPBar(37, int(float(MemPercentage)))
+    drawPBar(37, int(MemPercentage))
     draw.text((x, top + 45), Disk, font=font, fill=255)
     draw.text((x, top + 54), "Temp: " + Temp, font=font, fill=255)
 
-
-    # Display image.
     disp.image(image)
     disp.show()
     time.sleep(1)
